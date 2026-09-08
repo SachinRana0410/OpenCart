d3e943f290cf044e2bb1524bc42b59a6ef1c1894
Sachin Rana <rana.sachin2006@gmail.com>
2026-09-08 15:59:51 +0530

    PlaywrightConfig updated with Comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,16 +39,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
+   /*
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -57,7 +48,7 @@ export default defineConfig({
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
-    // },
+    // },*/
 
     /* Test against branded browsers. */
     // {
