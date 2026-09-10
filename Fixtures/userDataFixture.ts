5e563d0f42fc07afb0ca2c1b5129ca176de52de5
Prince <prince@Princes-MacBook-Air.local>
2026-09-10 11:58:36 +0530

    usual

--- Fixtures/userDataFixture.ts
+++ Fixtures/userDataFixture.ts
@@ -8,7 +8,7 @@ type Fixture = {
 };
 
 export const test = base.extend<Fixture>({
-    userData:async ({}, use)=>{
+    userData: async ({}, use)=>{
         const data = {
             email : "guptaprince.ec102@gmail.com",
             password : "Nextiswh@t123",
