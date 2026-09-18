0b4796c484670eb6178524633c29cea4383fbcca
Prince <prince@Princes-MacBook-Air.local>
2026-09-18 16:47:32 +0530

    jenkinsfile added

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -2,7 +2,7 @@ import {Page, expect, test} from "@playwright/test";
 import { HomePage } from "../pages/HomePage"; 
 import { TestConfig } from "../test.config";     
 
-test("Go to HomePage and verify the title of the page", async ({page})=>{
+test("@moke Go to HomePage and verify the title of the page", async ({page})=>{
 
     let config = new TestConfig();
     const homePage = new HomePage(page);
