163ac578b41ac3b9e1529e12ccf50e3ece163d7c
Sachin Rana <rana.sachin2006@gmail.com>
2026-09-20 00:16:44 +0530

    remove only from negative test case and add assertion

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -16,15 +16,18 @@ test("@smoke Valid Login", async({page})=>{
     
 })
 
-test.only("@smoke @negative Invalid Login with Fixture", async({page, userData})=>{
+test("@smoke @negative Invalid Login with Fixture", async({page, userData})=>{
     let config = new TestConfig();
     let login = new LoginPage(page);
 
     await page.goto(config.appUrl);
     await login.goToLoginPage();
     await login.doLogin(userData.email, userData.password);
+    const failedLoginMsg = await login.getFailedLoginMsg();
 
     expect(await page.title()).toContain('Account Login');
     expect(page.url()).toContain('account/login');
+    expect(failedLoginMsg).toBe('Warning: No match for E-Mail Address and/or Password.');
+    await page.screenshot({path: 'tests/screenshots/InvalidLogin.png', fullPage: true});
     
 })
